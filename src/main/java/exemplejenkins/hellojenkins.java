bc09ee4253bacb366b8a4f5d40ad62736a7c2675
root <aminespankes@gmail.com>
2020-04-19 13:58:52 +0100

    #update error

--- src/main/java/exemplejenkins/hellojenkins.java
+++ src/main/java/exemplejenkins/hellojenkins.java
@@ -6,7 +6,7 @@ public class hellojenkins {
 		System.out.println("Hello, world!");
 		System.out.println("Hello, world!");
 		System.out.println("Hello, world!");
-		System.out.println("Hello, world!");
+		"Hello, world!");
 		
 		
 
